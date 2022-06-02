1c836ca10a4f7bbafcfd80e5ce3fc5547f0803e2
alirezabashi98 <alirezabashi98@gmail.com>
2022-06-02 08:34:37 +0430

    start project wikipedia

---
+++ app/src/main/java/com/example/wikipedia/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.wikipedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wikipedia.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarMain)
+    }
+}
