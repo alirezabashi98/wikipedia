7c1e0464af3bce3877b78ece7b41a3ac41c617a7
alirezabashi98 <alirezabashi98@gmail.com>
2022-06-04 13:20:33 +0430

    add bottom navigation

---
+++ app/src/main/java/com/example/wikipedia/fragments/ProfileFragment.kt
@@ -0,0 +1,28 @@
+package com.example.wikipedia.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wikipedia.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+    private lateinit var binding: FragmentProfileBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
