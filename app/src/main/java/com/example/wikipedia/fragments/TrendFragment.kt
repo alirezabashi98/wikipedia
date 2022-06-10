a7f003bf76fdfdceac6161c965b2cdb006d73b7e
alirezabashi98 <alirezabashi98@gmail.com>
2022-06-10 22:40:19 +0430

    create show complete explanations activity

--- app/src/main/java/com/example/wikipedia/fragments/TrendFragment.kt
+++ app/src/main/java/com/example/wikipedia/fragments/TrendFragment.kt
@@ -1,6 +1,7 @@
 package com.example.wikipedia.fragments
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wikipedia.ShowCompleteExplanationsActivity
 import com.example.wikipedia.adapter.ExploreAdapter
 import com.example.wikipedia.adapter.ItemEvents
 import com.example.wikipedia.adapter.TrendAdapter
@@ -44,7 +46,7 @@ class TrendFragment : Fragment(),ItemEvents {
     }
 
     override fun onItemClicked(itemPost: ItemPostModel) {
-        toast(itemPost.Title)
+        startActivity(Intent(requireActivity(), ShowCompleteExplanationsActivity::class.java))
     }
 
     override fun onItemLongClicked(itemPost: ItemPostModel) {
