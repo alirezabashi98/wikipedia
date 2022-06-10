93407455e71774adfa2db419f57d2600e11e06a4
alirezabashi98 <alirezabashi98@gmail.com>
2022-06-10 22:30:40 +0430

    create show complete explanations activity

--- app/src/main/java/com/example/wikipedia/ShowCompleteExplanationsActivity.kt
+++ app/src/main/java/com/example/wikipedia/ShowCompleteExplanationsActivity.kt
@@ -1,11 +1,34 @@
 package com.example.wikipedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.wikipedia.databinding.ActivityShowCompleteExplanationsBinding
 
 class ShowCompleteExplanationsActivity : AppCompatActivity() {
+    lateinit var binding: ActivityShowCompleteExplanationsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_complete_explanations)
+        binding = ActivityShowCompleteExplanationsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolBarShowComplete)
+        binding.collapsingToolBar.setExpandedTitleColor(
+            ContextCompat.getColor(
+                this,
+                android.R.color.transparent
+            )
+        )
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressed()
+            }
+        }
+        return true
     }
 }
